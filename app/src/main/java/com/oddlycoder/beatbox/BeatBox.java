e40d102a0e35ccd9ab8ddca2f4b0550014ec246a
oddlycoder <awuahemmanuel56@gmail.com>
2020-02-27 03:09:00 +0000

    sound assets handler

---
+++ app/src/main/java/com/oddlycoder/beatbox/BeatBox.java
@@ -0,0 +1,44 @@
+package com.oddlycoder.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private AssetManager mAssets;
+
+    private List<Sound> mSounds = new ArrayList<Sound>();
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "loadSounds: Sounds found: " + soundNames.length);
+        } catch (IOException ioe) {
+            Log.e(TAG, "loadSounds: could not list assets ", ioe);
+            return;
+        }
+
+        for (String filename : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+
+}
