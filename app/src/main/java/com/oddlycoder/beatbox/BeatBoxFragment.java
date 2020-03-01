7edb31acf35a668353892f386f6999c38830eff0
oddlycoder <awuahemmanuel56@gmail.com>
2020-03-01 23:42:09 +0000

    binding sound to viewModel

--- app/src/main/java/com/oddlycoder/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/oddlycoder/beatbox/BeatBoxFragment.java
@@ -55,6 +55,12 @@ public class BeatBoxFragment extends Fragment {
         private SoundHolder(ListItemSoundBinding soundBinding) {
             super(soundBinding.getRoot());
             this.soundBinding = soundBinding;
+            this.soundBinding.setViewModel(new SoundViewModel(mBeatBox));
+        }
+
+        public void bind(Sound sound) {
+            soundBinding.getViewModel().setSound(sound);
+            soundBinding.executePendingBindings();
         }
     }
 
@@ -77,7 +83,8 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
-            
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
         }
 
         @Override
