d8c5b08965b12614c91f3a7fcbc8b89db33e02a3
oddlycoder <awuahemmanuel56@gmail.com>
2020-03-02 00:17:33 +0000

    handled load sounds exception

--- app/src/main/java/com/oddlycoder/beatbox/BeatBox.java
+++ app/src/main/java/com/oddlycoder/beatbox/BeatBox.java
@@ -46,9 +46,14 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "loadSounds: Could not load sounds: ", ioe);
+            }
         }
     }
 
